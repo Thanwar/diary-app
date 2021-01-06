8a9214bf73bc78aa39c29119194770e795b91429
Thanwar <thawardas25@gmail.com>
2021-01-06 23:33:55 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { setupServer } from './services/mirage/server';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { setupServer } from "./services/mirage/server";
 
+import { Provider } from "react-redux";
+import store from "./store";
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   setupServer();
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
